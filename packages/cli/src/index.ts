b8601731eb3b1a48a8cc3aaef0d5d5fdb9db271a
Li Kai <hello+github@lik.ai>
2019-07-08 23:46:46 +0800

    chore: upgrade dependencies

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world')
+import program from 'commander'
+import packageJson from '../package.json'
+
+program.command('start <file>').action(function(dir, cmd) {
+  console.log('remove ' + dir + (cmd.recursive ? ' recursively' : ''))
+})
+
+program.command('build <file>').action(function(dir, cmd) {
+  console.log('remove ' + dir + (cmd.recursive ? ' recursively' : ''))
+})
+
+program.version(packageJson.version).parse(process.argv)
