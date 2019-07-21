6c76df4d7abb4cf3c7e36444ce3d304b8513b64f
Li Kai <hello+github@lik.ai>
2019-07-21 12:11:00 +0800

    feat: render html using jsx instead of string

--- examples/preact-web-app/src/index.js
+++ examples/preact-web-app/src/index.js
@@ -2,55 +2,68 @@ import render from 'preact-render-to-string'
 import { h } from 'preact'
 import App from './App.js'
 
-function generateCSSReferences(files = [], publicPath = '') {
-  return files
-    .map(file => `<link href="${publicPath}${file}" rel="stylesheet">`)
-    .join('')
-}
-
-function generateJSReferences(files = [], publicPath = '') {
-  return files
-    .map(file => `<script src="${publicPath}${file}" nomodule defer></script>`)
-    .join('')
-}
-
-function generateMJSReferences(files = [], publicPath = '') {
-  return files
-    .map(file => `<script src="${publicPath}${file}" type=module></script>`)
-    .join('')
-}
-
 function defaultTemplate({
   css,
   js,
   mjs,
   title = 'Example Preact Web App',
   htmlAttributes = { lang: 'en' },
-  publicPath,
 }) {
-  const normalizedPublicPath = publicPath || ''
   return `<!DOCTYPE html>
-  <html ${Object.entries(htmlAttributes || {})
-    .map(attribute => `${attribute[0]}="${attribute[1]}"`)
-    .join(' ')}>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>${title || ''}</title>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
-      ${generateCSSReferences(css, normalizedPublicPath)}
-      ${generateMJSReferences(mjs, normalizedPublicPath)}
-      ${generateJSReferences(js, normalizedPublicPath)}
-    </head>
-    <body>
-      <div id="root">
-      ${render(<App />)}
-      </div>
-    </body>
-  </html>`
+  ${render(
+    <html {...htmlAttributes}>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{title}</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${__webpack_public_path__}apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${__webpack_public_path__}favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${__webpack_public_path__}favicon-16x16.png`}
+        />
+        <link
+          rel="manifest"
+          href={`${__webpack_public_path__}site.webmanifest`}
+        />
+        {css.map(cssResourceUrl => (
+          <link
+            href={`${__webpack_public_path__}${cssResourceUrl}`}
+            rel="stylesheet"
+          />
+        ))}
+        {js.map(jsResourceUrl => (
+          <script
+            src={`${__webpack_public_path__}${jsResourceUrl}`}
+            nomodule
+            defer
+          />
+        ))}
+        {mjs.map(mjsResourceUrl => (
+          <script
+            src={`${__webpack_public_path__}${mjsResourceUrl}`}
+            type="module"
+          />
+        ))}
+      </head>
+      <body>
+        <div id="root">
+          <App />
+        </div>
+      </body>
+    </html>,
+  )}`
 }
 
 module.exports = defaultTemplate
--- examples/react-web-app/src/index.js
+++ examples/react-web-app/src/index.js
@@ -2,55 +2,73 @@ import React from 'react'
 import ReactDOM from 'react-dom/server'
 import App from './App.js'
 
-function generateCSSReferences(files = [], publicPath = '') {
-  return files
-    .map(file => `<link href="${publicPath}${file}" rel="stylesheet">`)
-    .join('')
-}
-
-function generateJSReferences(files = [], publicPath = '') {
-  return files
-    .map(file => `<script src="${publicPath}${file}" nomodule defer></script>`)
-    .join('')
-}
-
-function generateMJSReferences(files = [], publicPath = '') {
-  return files
-    .map(file => `<script src="${publicPath}${file}" type=module></script>`)
-    .join('')
-}
-
 function defaultTemplate({
   css,
   js,
   mjs,
   title = 'Example React Web App',
   htmlAttributes = { lang: 'en' },
-  publicPath,
 }) {
-  const normalizedPublicPath = publicPath || ''
+  // First we render the outside parts of html as static string (e.g. head, body)
+  // Next, we render the web app root as html (e.g. <App />) and
+  // inject this into the outside parts via dangerouslySetInnerHTML because
+  // we only need React to hydrate the root element.
   return `<!DOCTYPE html>
-  <html ${Object.entries(htmlAttributes || {})
-    .map(attribute => `${attribute[0]}="${attribute[1]}"`)
-    .join(' ')}>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>${title || ''}</title>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
-      ${generateCSSReferences(css, normalizedPublicPath)}
-      ${generateJSReferences(js, normalizedPublicPath)}
-      ${generateMJSReferences(mjs, normalizedPublicPath)}
-    </head>
-    <body>
-      <div id="root">
-      ${ReactDOM.renderToString(<App />)}
-      </div>
-    </body>
-  </html>`
+  ${ReactDOM.renderToStaticMarkup(
+    <html {...htmlAttributes}>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{title}</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${__webpack_public_path__}apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${__webpack_public_path__}favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${__webpack_public_path__}favicon-16x16.png`}
+        />
+        <link
+          rel="manifest"
+          href={`${__webpack_public_path__}site.webmanifest`}
+        />
+        {css.map(cssResourceUrl => (
+          <link
+            href={`${__webpack_public_path__}${cssResourceUrl}`}
+            rel="stylesheet"
+          />
+        ))}
+        {js.map(jsResourceUrl => (
+          <script
+            src={`${__webpack_public_path__}${jsResourceUrl}`}
+            nomodule
+            defer
+          />
+        ))}
+        {mjs.map(mjsResourceUrl => (
+          <script
+            src={`${__webpack_public_path__}${mjsResourceUrl}`}
+            type="module"
+          />
+        ))}
+      </head>
+      <body>
+        <div
+          id="root"
+          dangerouslySetInnerHTML={{ __html: ReactDOM.renderToString(<App />) }}
+        />
+      </body>
+    </html>,
+  )}`
 }
 
 module.exports = defaultTemplate
