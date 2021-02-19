b53d6570dafb6f055a7cfd101f6303b6ad1a34a4
Li Kai <hello+github@lik.ai>
2021-02-19 13:43:10 +0800

    chore: upgrade dev dependencies

--- packages/babel-webpack-plugin/src/types/DynamicEntryPlugin.d.ts
+++ packages/babel-webpack-plugin/src/types/DynamicEntryPlugin.d.ts
@@ -1,6 +1,7 @@
 declare module 'webpack/lib/DynamicEntryPlugin' {
   import { Plugin } from 'webpack'
   export default class DynamicEntryPlugin extends Plugin {
+    // @ts-ignore use Function cause that's what it is
     public constructor(context: string, entry: Function)
   }
 }
